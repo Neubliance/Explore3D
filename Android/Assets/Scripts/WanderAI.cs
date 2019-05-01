7b36c8b413d6fba1a55a5cd015b4728e0f12851a
Neubliance <ds.kilgore02@gmail.com>
2019-05-01 04:06:39 -0400

    Updated Android

---
+++ Android/Assets/Scripts/WanderAI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderAI : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public float rotationSpeed = 100f;
+
+    private bool isWanderdering = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWanderdering == false) {
+            StartCoroutine(Wander());
+        }
+        if (isRotatingRight == true) {
+            transform.Rotate(transform.up * Time.deltaTime * rotationSpeed);
+        } 
+        if (isRotatingLeft == true) {
+            transform.Rotate(transform.up * Time.deltaTime * -rotationSpeed);
+        }
+        if (isWalking == true) {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    IEnumerator Wander()
+    {
+        int rotationTime = Random.Range(1, 2);
+        int rotateWait = Random.Range(1, 2);
+        int rotateLorR = Random.Range(0, 2);
+        int walkWait = Random.Range(1, 3);
+        int walkTime = Random.Range(1, 3);
+
+        isWanderdering = true;
+
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+        yield return new WaitForSeconds(rotateWait);
+        if (rotateLorR == 1) {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotationTime);
+            isRotatingRight = false;
+        }
+        if (rotateLorR == 2) {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotationTime);
+            isRotatingLeft = false;
+        }
+        isWanderdering = false;
+    }
+}
